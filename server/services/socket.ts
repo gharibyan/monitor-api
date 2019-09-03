8b2ad833bbcddb20b75bbba655191ab23538645b
gharibyan <khachatur.gharibyan@gmail.com>
2019-09-03 22:42:37 +0400

    send avg 30 second notification.

--- server/services/socket.ts
+++ server/services/socket.ts
@@ -6,9 +6,12 @@ const slack = new Slack();
 
 const INTERVAL = process.env.INTERVAL || 1000;
 
+
+
 class Socket {
     io: any;
     timer: any;
+    buffer: Array<number>
 
     constructor(http: any) {
         this.io = require("socket.io")(http, {
@@ -17,6 +20,7 @@ class Socket {
             }
         });
         this.timer = false;
+        this.buffer = []
     }
 
     init() {
@@ -31,9 +35,23 @@ class Socket {
             const usage: any | number = await Os.cpuUsage();
             const free = await Os.cpuFree();
             const d = {...Os.infos, cpuUsage: usage, cpuFree: free};
-            if (usage >= 0.8 && slack.slackActive) {
-                slack.send(`CPU USAGE IS: ${(usage * 100).toFixed(2)}%`)
+            if (usage >= 0.8) {
+                this.buffer.push(usage);
             }
+            if (this.buffer.length >= 20) {
+                const sum: number = this.buffer.reduce((a, b) => a + b, 0);
+                const avg: number = sum / this.buffer.length;
+                const high: number = Math.max(...this.buffer);
+                const low: number = Math.min(...this.buffer);
+                if (slack.slackActive) {
+                    const avgText:string = `CPU avg: ${(avg * 100).toFixed(2)}%`;
+                    const highText:string = `high: ${(high * 100).toFixed(2)}%`;
+                    const lowText:string = `low: ${(low * 100).toFixed(2)}%`;
+                    slack.send(`${avgText} ${highText} ${lowText}`)
+                }
+                this.buffer = [];
+            }
+
             if (Object.keys(this.io.sockets.sockets).length) {
                 this.io.sockets.emit('serverHealthCheck', d);
             }
