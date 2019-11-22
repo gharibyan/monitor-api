f8de9e28c3b6227a9fc7d8100ef78ee5e3d5f2f1
gharibyan <khachatur.gharibyan@gmail.com>
2019-11-22 10:46:44 +0400

    add folder listener, now if there is new file it will handle new file and send info.

--- server/services/fileReader.ts
+++ server/services/fileReader.ts
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const readLastLines = require('read-last-lines');
+
 interface Line {
-    file:String,
-    data:Array<string>|null
+    file: String,
+    data: Array<string> | null
 }
 
 import Slack from './slack';
@@ -17,8 +18,8 @@ class FileReader {
     }
 
     async readLines(file: string, lineNumbers: number) {
-        const lines:String = await readLastLines.read(`${this.filePath}${file}`, lineNumbers);
-        const response:Line = {
+        const lines: String = await readLastLines.read(`${this.filePath}${file}`, lineNumbers);
+        const response: Line = {
             file: file,
             data: lines ? lines.split('\n') : null
         };
@@ -38,19 +39,15 @@ class FileReader {
     }
 
     listen(cb: Function) {
-        const files: Array<string> = fs.readdirSync(this.filePath);
-        console.log('listeningLogFiles', files);
-        for (const file of files) {
-            if (file.includes('.log')) {
-                fs.watchFile(`${this.filePath}${file}`, async () => {
-                    const lastLine: string = await readLastLines.read(`${this.filePath}${file}`, 1);
-                    if (file.includes('error')  && slack.slackActive) {
-                        slack.send(`error:${file} -- ${lastLine}`, 'bug')
-                    }
-                    cb({file, data:lastLine})
-                })
+        fs.watch(`${this.filePath}`, async (_: any, filename: any) => {
+            if (filename.includes('.log') && fs.existsSync(`${this.filePath}${filename}`)) {
+                const lastLine: string = await readLastLines.read(`${this.filePath}${filename}`, 1);
+                if (filename.includes('error') && slack.slackActive) {
+                    slack.send(`error:${filename} -- ${lastLine}`, 'bug')
+                }
+                cb({filename, data: lastLine})
             }
-        }
+        })
     }
 
 }
